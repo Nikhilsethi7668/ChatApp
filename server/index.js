dbaee7652caba7e5394ee2d2871003b0c9e22717
Nikhil Sethi <132539777+Nikhilsethi7668@users.noreply.github.com>
2024-12-07 01:59:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "*", // Allow requests only from the frontend
+    origin: "*", // Allow requests only from the frontend
     methods: ["GET", "POST"],
   })
 );
