42102e50634e91292de6b21d2491e9e2e97e21a7
Nikhil Sethi <132539777+Nikhilsethi7668@users.noreply.github.com>
2024-12-07 16:50:51 +0530

    Update upload.js

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -2,12 +2,12 @@ const multer = require("multer");
 const { GridFsStorage } = require("multer-gridfs-storage");
 const dotenv = require("dotenv");
 dotenv.config();
-const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4q2wn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4q2wn.mongodb.net/`;
 const storage = new GridFsStorage({
   url: url,
   options: { useNewUrlParser: true },
   file: (req, file) => {
-    const match = ["image/jpg", "image/png"];
+    const match = ["image/jpg", "image/png", "image/jpeg"];
     if (match.indexOf(file.mimetype) === -1) {
       return `${Date.now()}-blog-${file.originalname}`;
     }
